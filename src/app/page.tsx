f94f392959a72abee8efae0fde53500e86855894
Amit Gupta <guptaamitgupta819@gmail.com>
2024-04-20 20:34:52 +0530

    Herosection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Herosection from "@/components/Herosection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-white text-center">Start learning next js</h1>
+      {/* <h1 className="text-2xl text-white text-center">Start learning next js</h1> */}
+      <Herosection/>
     </main>
   );
 }
