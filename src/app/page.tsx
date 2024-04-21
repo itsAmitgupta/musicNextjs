fb068c48dba2b9193c436e38459ad7f60acda511
Amit Gupta <guptaamitgupta819@gmail.com>
2024-04-21 12:25:53 +0530

    grid bg and moving card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import Herosection from "@/components/Herosection";
+import TestimonialCards from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      {/* <h1 className="text-2xl text-white text-center">Start learning next js</h1> */}
+    <main className="min-h-screen bg-black/[0.96] antialiased ">
       <Herosection/>
       <FeaturedCourses/>
       <WhyChooseUs/>
+      <TestimonialCards/>
     </main>
   );
 }
